ce3851b955e737632d0b8c92be33846f14b47d83
Hao Guan <hguandl@gmail.com>
2021-02-15 18:20:19 +0800

    More detailed examples for config.

--- config-example.py
+++ config-example.py
@@ -1,1 +1,31 @@
+# Bark original API: https://api.day.app
+bark_api_tokens = [
+    'alice',
+    'bob',
+    'charlie'
+]
+
+
+# Bark custom API
 bark_custom_api = "http://localhost:8080"
+
+
+# IFTTT Webhook
+ifttt_webhook_infos = [
+    ("qWertYUioP", "new_weibo"), # (API key, event name)
+    ("zxcvbnmzxc", "arknights")
+]
+
+
+# Telegram Bot
+tg_bot_api = "123456:asdfghjkl"
+tg_chats = [
+    114514,
+    -1919810
+]
+
+
+# Work Wechat App
+corpid = "ww00112233"
+agentid = 1000002
+corpsecret = "ExamPLEseCrET"
